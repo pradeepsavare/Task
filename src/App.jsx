0ace146c7f58eee9b6c4793f044a3bc7fde386a1
pradeep savare <pradeepsaware1@gmail.com>
2026-01-21 18:08:13 +0530

    added image and content using api  make responsive page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ function App() {
   const { data, isLoading, isError } = useUsersQuery();
 
   if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Error fetching users</p>;
+  if (isError) return <p>Error Fetching Data</p>;
 
   return (
-    <div className="flex flex-wrap gap-4 p-4 justify-center items-center">
-      {data.users.map((user) => (
-        <div
-          key={user.id}
-          className="bg-blue-300 text-white text-center p-4 w-[45%] md:w-[25%] lg:w-[18%]"
-        >
-          <p className="text-gray-700 text-lg md:text-xl lg:text-2xl">{user.firstName} {user.lastName}</p>
-          <p className="text-base md:text-lg lg:text-xl">Age: {user.age}</p>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-3">
+      {data.map((user) => (
+        <div key={user.id} className="bg-blue-300 text-center p-4 rounded-lg hover:shadow-neutral-500 shadow-lg transition-shadow duration-200 cursor-pointer">
+         
+          <div className="w-[100%] h-35 mb-2 overflow-hidden">
+           <img
+            src={user.urls.regular}
+            alt={user.user.name}
+            className="w-full h-full object-cover rounded-2xl "
+          />
+          </div>
+          <h2 className="text-base md:text-xl lg:text-2xl font-bold mb-1 ">{user.user.name}</h2>
+          <p className="text-center text-sm md:text-base lg:text-lg font-semibold">{`Desc: ${user.alt_description}`}</p>
+          
         </div>
       ))}
     </div>
--- src/Users.js
+++ src/Users.js
@@ -8,10 +8,17 @@ const getUsers = async () => {
   return res.json();
 };
 
+const getImg = async () => {
+  const res = await fetch("https://api.unsplash.com/photos/?client_id=nH7ljacRcj8Q03gwWvO2DLiB3DRlyCFiM3P_YZLG6nc");
+  if (!res.ok){
+    throw new Error("Failed to fetch image");
+  }
+  return res.json();
+} 
 
 export const useUsersQuery = () => {
   return useQuery({
-    queryKey: ["users"],
-    queryFn: getUsers,
+    queryKey: ["imges"],
+    queryFn: getImg,
   });
 };
